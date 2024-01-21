2f430c4151857a344ba7cf396c51c647c681ab51
Mikeprog2022 <mikeprog22@gmail.com>
2024-01-21 16:04:09 +0300

    Form edit

--- links/forms.py
+++ links/forms.py
@@ -7,3 +7,11 @@ class LinkForm(forms.ModelForm):
         model = Link
         fields = ['author', 'title', 'date_published', 'url']
 
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'form-control bg-white border-md'}),
+            'title': forms.TextInput(attrs={'class': 'form-control bg-white border-md'}),
+            'date_published': forms.DateInput(attrs={'class': 'form-control bg-white border-md'}),
+            'url': forms.URLInput(attrs={'class': 'form-control bg-white border-md'}),
+        }
+
+
